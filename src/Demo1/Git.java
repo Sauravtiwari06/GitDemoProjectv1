b584f0f1fb942e85b7259284ab814d3be9fee867
saurav.tiwari <saurav.tiwari@P2ELT33.P2E.COM>
2024-10-28 15:13:13 +0530

    Commit First

--- src/Demo1/Git.java
+++ src/Demo1/Git.java
@@ -1,5 +1,13 @@
 package Demo1;
 
 public class Git {
+	public static void main(String[] args) {
+		String hungry = "Yes";
+		if(hungry=="yes") {
+			System.out.println("Eat Something ....");
+		}else {
+			System.out.println("Do Work ...");
+		}
+	}
 
 }
